17b8f661606c26f64f0fa764596d0d33d19b4d3d
dmartha-20 <131492091+dmartha-20@users.noreply.github.com>
2023-07-19 22:37:57 -0400

    database deployed

--- server/Config/database.js
+++ server/Config/database.js
@@ -1,16 +1,15 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  connectionLimit: 10,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   connectionLimit: 10,
+// });
 
-pool.getConnection(function (err, connection) {
-  console.log("DataBase Connected!");
-});
+const pool = mysql.createConnection(process.env.DATABASE_URL)
+
 
 let registration = `CREATE TABLE if not exists registration(
     user_id int auto_increment,
@@ -25,8 +24,8 @@ let profile = `CREATE TABLE if not exists profile(
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    PRIMARY KEY (user_profile_id), 
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    PRIMARY KEY (user_profile_id) 
+  
 )`;
 
 let question = `CREATE TABLE if not exists question(
@@ -38,8 +37,8 @@ let question = `CREATE TABLE if not exists question(
     post_id varchar(255) not null,
     user_id int not null,
     PRIMARY KEY (question_id),
-    UNIQUE KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+    UNIQUE KEY (post_id)
+  
 )`;
 
 let answer = `CREATE TABLE if not exists answer(
@@ -48,9 +47,8 @@ let answer = `CREATE TABLE if not exists answer(
     answer_code_block varchar(255),
     user_id int not null,
     question_id int not null, 
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id),
-    FOREIGN KEY (question_id) REFERENCES question(question_id)
+    PRIMARY KEY (answer_id)
+   
 )`;
 
 pool.query(registration, (err, results) => {
