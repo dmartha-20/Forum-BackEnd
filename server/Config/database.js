84c0c91175542b3435631e9ea1fa8d0f069817e5
dmartha-20 <131492091+dmartha-20@users.noreply.github.com>
2023-07-20 20:50:38 -0400

    env update

--- server/Config/database.js
+++ server/Config/database.js
@@ -1,13 +1,7 @@
 const mysql = require("mysql2");
 require('dotenv').config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.MYSQL_DB,
-//   connectionLimit: 10,
-// });
+
 
 const pool = mysql.createConnection(process.env.DATABASE_URL)
 
@@ -49,7 +43,7 @@ let answer = `CREATE TABLE if not exists answer(
     user_id int not null,
     question_id int not null, 
     PRIMARY KEY (answer_id)
-   
+
 )`;
 
 pool.query(registration, (err, results) => {
