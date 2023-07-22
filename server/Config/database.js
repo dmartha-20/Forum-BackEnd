6c7b6673f071ce1130503b6acc9dcab8a9e4d39c
dmartha-20 <131492091+dmartha-20@users.noreply.github.com>
2023-07-22 11:33:16 -0400

    start node server,js updated

--- server/Config/database.js
+++ server/Config/database.js
@@ -1,6 +1,15 @@
 const mysql = require("mysql2");
 require('dotenv').config();
 
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   connectionLimit: 10,
+// });
+
+
 
 const pool = mysql.createConnection(process.env.DATABASE_URL);
 
